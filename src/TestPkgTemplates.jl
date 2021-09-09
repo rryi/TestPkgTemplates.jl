74dad812d616e9a197ab35c28543b4c1026da41b
Robert Rudolph <rudolph.consulting@gmail.com>
2021-09-09 18:39:13 +0200

    coded and tested

--- src/TestPkgTemplates.jl
+++ src/TestPkgTemplates.jl
@@ -2,4 +2,13 @@ module TestPkgTemplates
 
 # Write your package code here.
 
+"""
+A minimally documented test function
+"""
+function testadd(a,b)
+    a+b
+end
+
+export testadd
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "TestPkgTemplates.jl" begin
     # Write your tests here.
+    @test testadd(1,2) == 3
 end
